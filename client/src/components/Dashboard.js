885648ca43383e8995d8d22a80a0c79de1eff4ed
Denis Molloy <denis.molloy@gmail.com>
2020-11-25 16:50:42 -0800

    submitting new messages

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import Sidebar from './Sidebar'
+import React from "react";
+import Sidebar from "./Sidebar";
+import OpenConversation from "./OpenConversation";
+import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function Dashboard({ id }) {
+    const { selectedConversation } = useConversations();
     return (
         <div className="d-flex" style={{ height: "100vh" }}>
             <Sidebar id={id} />
+            {selectedConversation ? (
+                <OpenConversation conversation={selectedConversation} />
+            ) : (
+                    "Loading..."
+                )}
         </div>
-    )
+    );
 }
